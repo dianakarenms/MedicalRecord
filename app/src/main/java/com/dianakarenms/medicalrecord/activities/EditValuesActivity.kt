e7fbd5cb3148cf6084b97c555a333f19679d9a7b
karenmunoz <dianakarenms@gmail.com>
2018-10-01 16:42:08 -0500

    Initial commit

---
+++ app/src/main/java/com/dianakarenms/medicalrecord/activities/EditValuesActivity.kt
@@ -0,0 +1,16 @@
+package com.dianakarenms.medicalrecord.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.dianakarenms.medicalrecord.R
+
+/**
+ * Created by diana.munoz on 10/1/18.
+ */
+class EditValuesActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_values)
+    }
+}
---
+++ app/src/main/java/com/dianakarenms/medicalrecord/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.dianakarenms.medicalrecord.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.dianakarenms.medicalrecord.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
