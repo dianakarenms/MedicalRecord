e115b27684b7205928ae15996bbb36f42f310017
karenmunoz <dianakarenms@gmail.com>
2018-10-02 16:59:13 -0500

    Added custom toolbar in MainActivity. Added Anko library. Start DoctorDataActivity from toolbar.

--- app/src/main/java/com/dianakarenms/medicalrecord/activities/MainActivity.kt
+++ app/src/main/java/com/dianakarenms/medicalrecord/activities/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.dianakarenms.medicalrecord.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.dianakarenms.medicalrecord.R
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(mainToolbar)
+        toolbarMainDoctorSettingsBtn.setOnClickListener {
+            startActivity<DoctorDataActivity>()
+        }
     }
 }
